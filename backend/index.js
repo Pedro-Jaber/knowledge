0269a568a5653b467801f220d304939cd368a2dc
Pedro Jaber <pedrojaberc@gmail.com>
2025-09-15 21:23:27 -0300

    14/09

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+/*
+npm i bcrypt consign cors express jwt-simple knex moment mongoose node-schedule node-schedule passport passport-jwt pg pm2
+*/
+
+require("dotenv").config();
+const app = require("express")();
+const consign = require("consign");
+
+const db = require("./config/db");
+
+//* Dotenv
+const PORT = process.env.PORT || 3000;
+
+//* Config
+app.db = db;
+consign()
+  .then("./config/middlewares.js")
+  .then("./api")
+  .then("./config/routes.js")
+  .into(app);
+
+app.listen(PORT, () => {
+  console.log("┌─────────────────────────────┐");
+  console.log("│ Server:\x1b[92m Online \x1b[0m             │");
+  console.log(`│ Port: ${PORT}                  │`);
+  console.log(`│ link: http://localhost:${PORT} │`);
+  console.log("└─────────────────────────────┘");
+});
---
+++ backend/knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  client: "postgresql",
+  connection: {
+    database: "knowledge",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+  },
+};
